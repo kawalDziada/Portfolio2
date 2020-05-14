80eeb08c0c17965fca96230052f99cd0de6b362c
Marcin Kondol <marcinkondol@Marcins-MacBook-Pro.local>
2020-05-14 13:16:43 +0100

    iframe added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Route, Switch, BrowserRouter} from 'react-router-dom';
 
 import NavBar from './Components/NavBar'
@@ -9,16 +9,19 @@ import Footer from './Components/Footer';
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <NavBar />
-          <Switch>
-            <Route exact path="/" component={MainPortfolio} />
-            <Route path="/About" component={About} />
-          </Switch>
-          <Footer />
-        </div>
-      </BrowserRouter>
+      <Fragment>
+        <BrowserRouter>
+          <div>
+            <NavBar />
+            <Switch>
+              <Route exact path="/" component={MainPortfolio} />
+              <Route path="/About" component={About} />
+            </Switch>
+            <Footer />
+          </div>
+        </BrowserRouter>
+        <iframe src="https://tokbox.com/embed/embed/ot-embed.js?embedId=33ce89a8-6a4c-407f-81e1-20db465d6c54&room=DEFAULT_ROOM&iframe=true" width="800px" height="640px" scrolling="auto" allow="microphone; camera" ></iframe>
+      </Fragment>
     );
   }
 }
